ec6d4d3ffe029e576b5c7974dee535323647c061
adanroaf <cadanroaf@gmail.com>
2026-06-05 16:45:01 -0300

    subida de actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
-    
+    const { order, addItem } = useOrder()
   return (
     <>
       <header className="bg-teal-400 py-5">
@@ -10,19 +12,22 @@ function App() {
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>  
-          <h2>Menú</h2>
-          {menuItems.map(item => (
-            <MenuItem
-              key={item.id}
-              item={item}
-            
-            
-            />
-          ))}
+        <div className="space-y-5">  
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
         </div>
-        <div>
-          <h2 className="">Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-l-lg space-y-10">
+          <OrderContents 
+            order={order}
+          />
         </div>
       </main>
     </>
